d168aa0db8b9b68cce91b15d472761fe99adcf60
local-svg-map-maker <local-svg-map-maker@tools.wmflabs.org>
2014-03-03 18:45:39 +0000

    Adding error reporting and fixing index

--- mapgenerate.php
+++ mapgenerate.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 function createPNG( $svgfile, $pngfile ) {
 	$command = "rsvg " . $svgfile . " " . $pngfile;
 	$make_map = system( $command, $retval );
