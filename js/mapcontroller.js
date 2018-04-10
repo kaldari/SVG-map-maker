30f20caf09bc5277c4994dce808c19f944d9ae66
Kaldari <kaldari@gmail.com>
2018-04-09 21:01:09 -0700

    Fixing for Firefox and mobile

--- js/mapcontroller.js
+++ js/mapcontroller.js
@@ -2,7 +2,7 @@ color = "#D3D3D3";
 
 $(document).ready( function() {
 	$('.clickable').click( function(){
-		this.style.fill = color;
+		$( this ).attr( "fill", "#" + color );
 		$('#' + this.id + 'input').val( color );
 	} );
 } );
