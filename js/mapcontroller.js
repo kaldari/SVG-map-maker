6f8dd2445ceb6236e54832dbc0fb7ae8dcdc66fd
Kaldari <kaldari@gmail.com>
2018-05-19 14:45:07 +0200

    adding gradient color scheme

--- js/mapcontroller.js
+++ js/mapcontroller.js
@@ -20,6 +20,38 @@ function toggleTerritories() {
 	}
 }
 
+function toggleColors() {
+	if ( $( '#switcher' ).text() === 'Switch to gradient' ) {
+		$( '#color1' ).val( '15534C' );
+		$( '#color2' ).val( '297159' );
+		$( '#color3' ).val( '498F60' );
+		$( '#color4' ).val( '73AC61' );
+		$( '#color5' ).val( 'A6C760' );
+		$( '#color6' ).val( 'E2E062' );
+		$( '#color7' ).val( 'FCE77C' );
+		$( '#color8' ).val( 'D3D3D3' );
+		$( '.jscolor' ).each( function() {
+			$( this ).focus();
+			$( this ).blur();
+		} );
+		$( '#switcher' ).text( 'Switch to assortment' );
+	} else {
+		$( '#color1' ).val( '8DD3C7' );
+		$( '#color2' ).val( 'B3DE69' );
+		$( '#color3' ).val( 'BEBADA' );
+		$( '#color4' ).val( 'FB8072' );
+		$( '#color5' ).val( '80B1D3' );
+		$( '#color6' ).val( 'FCCDE5' );
+		$( '#color7' ).val( 'FDB462' );
+		$( '#color8' ).val( 'D3D3D3' );
+		$( '.jscolor' ).each( function() {
+			$( this ).focus();
+			$( this ).blur();
+		} );
+		$( '#switcher' ).text( 'Switch to gradient' );
+	}
+}
+
 function paintTerritory( territory ) {
 	if ( $( 'input#territories' ).prop( 'checked' ) ) {
 		$( '#' + territory ).attr( "fill", "#" + color );
